6a477df0067609bf1fcda20fb8871dbbe14f3d65
Murilo C. Seidenstucker <murilocampos.004@gmail.com>
2024-02-15 15:41:36 -0300

    register search and print

--- main.py
+++ main.py
@@ -1,20 +1,47 @@
+import json
+import re
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from time import sleep
 from urllib.parse import urlparse
 
-process_number = -1
+chrome_options = webdriver.ChromeOptions()
+settings = {
+       "recentDestinations": [{
+            "id": "Save as PDF",
+            # "id": "\\\\farmacia\\HP LaserJet Professional M1212nf MFP_1/local/",
+            "origin": "local",
+            "account": "",
+        }],
+        "selectedDestinationId": "Save as PDF",
+        "version": 2
+    }
+prefs = {'printing.print_preview_sticky_settings.appState': json.dumps(settings)}
+chrome_options.add_experimental_option('prefs', prefs)
+chrome_options.add_argument('--kiosk-printing')
+
+def resize_window(driver):
+    driver.maximize_window()
+    screen_width = driver.execute_script("return window.screen.availWidth")
+    screen_height = driver.execute_script("return window.screen.availHeight")
+    new_position = (screen_width // 2, 0)
+    new_size = (screen_width // 2, screen_height)
+    driver.set_window_position(*new_position)
+    driver.set_window_size(*new_size)
+
+process_number = ''
 anvisa_medicamento_url = r'https://consultas.anvisa.gov.br/#/medicamentos/'
 
-driver = webdriver.Chrome()
+driver = webdriver.Chrome(options=chrome_options)
 
 driver.get("https://www.google.com/")
-driver.maximize_window()
+resize_window(driver)
 driver.implicitly_wait(0.5)
 
-# name = input("Qual o nome do medicamento? -> ")
-# brand = input("Qual a marca do medicamento? -> ")
-name = "aciclovir"
-brand = "cimed"
+name = input("Qual o nome do medicamento? -> ")
+brand = input("Qual a marca do medicamento? -> ")
+# name = "aciclovir"
+# brand = "cimed"
 
 text_box_xpath = r'//*[@id="APjFqb"]'
 text_box = driver.find_element(by=By.XPATH, value=text_box_xpath)
@@ -37,16 +64,14 @@ for element in a_elements:
 
 driver.implicitly_wait(0.5)
 dataset = driver.find_element(By.CLASS_NAME, "dataset")
-a_elements_smerp = dataset.find_elements(By.TAG_NAME, "a")
-for element in a_elements_smerp:
-    if "consultar diretamente na anvisa" in element.text:
-        element.click()
-        break
+text = dataset.find_element(By.XPATH, '/html/body/section/center/article/div[1]/div[6]/div[2]').text
+reg2 = r'\D'
+process_number = re.sub(reg2, '', text)
 
-frame = driver.switch_to.frame('aswift_12')
-driver.switch_to.frame('ad_iframe')
-driver.find_element(By.ID, "dismiss-button").click()
-driver.current_url
+driver.get(rf'{anvisa_medicamento_url}{process_number}/')
+sleep(3)
+driver.execute_script('window.print();')
 
 # input()
 # driver.quit()
+
