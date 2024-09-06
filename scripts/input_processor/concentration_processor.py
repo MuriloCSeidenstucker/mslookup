cfa40fbe9b05a41901328f3be0e952bb897be423
Murilo Seidenstucker <murilocampos.004@gmail.com>
2024-09-06 16:39:14 -0300

    refactor: concentration_processor created

---
+++ scripts/input_processor/concentration_processor.py
@@ -0,0 +1,16 @@
+import re
+
+from scripts.json_manager import load_json
+
+class ConcentrationProcessor:
+    def __init__(self) -> None:
+        self.patterns_path = 'patterns.json'
+        self.patterns = load_json(self.patterns_path)
+    
+    def get_concentration(self, description: str) -> str:
+        lower_description = description.lower()
+        for pattern in self.patterns["patterns"]:
+            match = re.search(pattern, lower_description)
+            if match:
+                return match.group()
+        return "Concentração não encontrada"
--- scripts/input_processor/data_processor.py
+++ scripts/input_processor/data_processor.py
@@ -1,29 +1,17 @@
-import re
-
 from typing import List, Dict, Any
 
-from scripts.json_manager import load_json
 from scripts.input_processor.brand_processor import BrandProcessor
 from scripts.input_processor.description_processor import DescriptionProcessor
+from scripts.input_processor.concentration_processor import ConcentrationProcessor
 
 class DataProcessor:
     def __init__(self, checkpoint_manager):
         self.brand_processor = BrandProcessor()
         self.description_processor = DescriptionProcessor()
+        self.concentration_processor = ConcentrationProcessor()
         
         self.checkpoint_interval = 10
         self.checkpoint_manager = checkpoint_manager
-        
-        self.patterns_path = 'patterns.json'
-        self.patterns = load_json(self.patterns_path)
-        
-    def get_concentration(self, description: str, patterns: List[str]) -> str:
-        lower_description = description.lower()
-        for pattern in patterns["patterns"]:
-            match = re.search(pattern, lower_description)
-            if match:
-                return match.group()
-        return "Concentração não encontrada"
 
     def process_input(self, filtered_input: List[Dict[str, str]]) -> List[Dict[str, Any]]:
         data = []
@@ -39,7 +27,7 @@ class DataProcessor:
         for index, row in enumerate(filtered_input[start_index:]):
             brand = self.brand_processor.get_brand(row['brand'])
             filtered_description = self.description_processor.get_filtered_description(row['description'])
-            concentration = self.get_concentration(row['description'], self.patterns)
+            concentration = self.concentration_processor.get_concentration(row['description'])
             data.append({
                 'item': row['item'],
                 'origin_description': row['description'],
