363b6aacfa2a588d3c4d1ac3cfdb9b5217ef7d44
Murilo Seidenstucker <murilocampos.004@gmail.com>
2024-09-10 10:30:55 -0300

    todo updated

--- src/services/input_processor_service.py
+++ src/services/input_processor_service.py
@@ -1,7 +1,4 @@
-import pandas as pd
-
 from typing import Any, Dict, List
-
 from src.input_processors.input_processor import InputProcessor
 
 class InputProcessorService:
@@ -10,7 +7,5 @@ class InputProcessorService:
     
     def get_processed_input(self, raw_input: Dict[str, str]) -> List[Dict[str, Any]]:
         processed_input = []
-        
         processed_input = self.data_processor.process_input(raw_input)
-        
         return processed_input
