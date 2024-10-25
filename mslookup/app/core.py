caf439a32e70573e282cb9c5c8614783d5e89ec1
Murilo Seidenstucker <murilocampos.004@gmail.com>
2024-10-25 16:39:04 -0300

    refactor: logger config updated

--- mslookup/app/core.py
+++ mslookup/app/core.py
@@ -1,6 +1,8 @@
+import logging
 from mslookup.app.checkpoint_manager import CheckpointManager
 from mslookup.app.config import load_config
 from mslookup.app.input_manager import InputManager
+from mslookup.app.logger_config import configure_logging
 from mslookup.app.report_generator import ReportGenerator
 from mslookup.app.services.input_processor_service import InputProcessorService
 from mslookup.app.services.product_registration_service import \
@@ -10,7 +12,9 @@ from mslookup.app.services.registration_pdf_service import RegistrationPDFServic
 
 class Core:
     def __init__(self):
-
+        configure_logging()
+        self.name = self.__class__.__name__
+        logging.info(f'{self.name}: Inicializando app...')
         pdf_manager, anvisa_domain = load_config()
 
         self.input_manager = InputManager()
@@ -31,17 +35,25 @@ class Core:
             processed_input = self.input_processor_service.get_processed_input(
                 raw_input
             )
+            logging.info(f'{self.name}: Entrada de dados processada.')
+            
             product_registrations = (
                 self.product_registration_service.get_product_registrations(
                     processed_input
                 )
             )
+            logging.info(f'{self.name}: Registros dos produtos coletados.')
+            
             final_result = (
                 self.registration_pdf_service.generate_registration_pdfs(
                     product_registrations
                 )
             )
+            logging.info(f'{self.name}: Finalizado geração dos PDFs.')
+            
             self.report_generator.generate_report(final_result)
+            logging.info(f'{self.name}: Relatório gerado.')
+            
             self.all_stages_completed = True
         finally:
             if self.all_stages_completed:
--- mslookup/app/logger_config.py
+++ mslookup/app/logger_config.py
@@ -1,28 +1,60 @@
-import logging
 import os
+import logging
+from datetime import datetime
+from pathlib import Path
+import sys
 
-
-def setup_logger(
-    name: str, log_file: str, level: int = logging.INFO
-) -> logging.Logger:
-    """Configura e retorna um logger com o nome e arquivo de log especificados."""
-    log_dir = 'logs'
+def configure_logging():
+    # Criação do manipulador de log rotativo com base na data
+    date = datetime.now().strftime("%d-%m-%Y")
+    file_name = f"{date}_logs.log"
+    log_dir = 'logs'  # Diretório para os arquivos de log
+    full_file_path = os.path.join(log_dir, file_name)
+    
+    # Cria o diretório se não existir
     os.makedirs(log_dir, exist_ok=True)
-
-    log_path = os.path.join(log_dir, log_file)
-
-    handler = logging.FileHandler(log_path, mode='w')
-    handler.setLevel(level)
-    formatter = logging.Formatter(
-        '%(asctime)s - %(levelname)s - %(name)s - %(message)s'
+    
+    # Configuração do FileHandler com codificação 'utf-8'
+    file_handler = logging.FileHandler(full_file_path, mode='a', encoding='utf-8')
+    
+    # Define os padrões do Log
+    logging.basicConfig(
+        level=logging.INFO,
+        encoding="utf-8",
+        format="%(asctime)s - %(levelname)s - %(message)s",
+        datefmt="%d-%m-%Y %H:%M:%S",
+        handlers=[
+            file_handler,
+            logging.StreamHandler(),
+        ],
     )
-    handler.setFormatter(formatter)
-
-    logger = logging.getLogger(name)
-    logger.setLevel(level)
-    logger.addHandler(handler)
-
-    return logger
-
-
-main_logger = setup_logger('main_logger', 'project.log')
+    
+def configure_test_logging():
+    # Criação do manipulador de log rotativo com base na data
+    date = datetime.now().strftime("%d-%m-%Y")
+    file_name = f"{date}_test_logs.log"
+    log_dir = r'logs\test'  # Diretório para os arquivos de log
+    full_file_path = os.path.join(log_dir, file_name)
+    
+    # Cria o diretório se não existir
+    os.makedirs(log_dir, exist_ok=True)
+    
+    # Configuração do FileHandler com codificação 'utf-8'
+    file_handler = logging.FileHandler(full_file_path, mode='a', encoding='utf-8')
+    
+    # Define os padrões do Log
+    logging.basicConfig(
+        level=logging.INFO,
+        encoding="utf-8",
+        format="%(asctime)s - %(levelname)s - %(message)s",
+        datefmt="%d-%m-%Y %H:%M:%S",
+        handlers=[
+            file_handler,
+            logging.StreamHandler(),
+        ],
+    )
+    
+def base_path( file_path: str):
+    if getattr(sys, 'frozen', False) and hasattr(sys, '_MEIPASS'):
+        return Path(f'{sys._MEIPASS}/{file_path}')
+    return Path(file_path)
